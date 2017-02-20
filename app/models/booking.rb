d4a5553ca36c40a8c47cdd97f63b2f4dfce80021
stephen choi <stephenjhchoi@gmail.com>
2017-02-20 16:44:41 +0000

    fixed models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :lesson
 
   validates :location, presence: true
-  validates :price, presence: true
-  validates :date, presence: true
-  validates :duration, presence: true
-  validates :description, presence: true
-  validates :neighborhood, presence: true
 end
