39254d85da8be3f1166349a3f04fb61061bddaf1
stephen choi <stephenjhchoi@gmail.com>
2017-02-21 19:18:20 +0000

    changed search and selector

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,7 +1,7 @@
 class Lesson < ApplicationRecord
   belongs_to :user
   has_one :booking
-
+  CATEGORIES = ["Yoga", "Cardio", "Pilates", "Weight Lifting", "Boxing", "Tennis"]
   validates :category, presence: true
   validates :price, presence: true
   validates :start_date_time, presence: true
