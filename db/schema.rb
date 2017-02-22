36fcfcaa7a25641c8ebe69a8501c27990c36b8b5
Allison Grekin <allison_grekin@hotmail.com>
2017-02-22 17:20:38 +0000

    worked on css/home page/create lesson form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221172409) do
+ActiveRecord::Schema.define(version: 20170222115436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20170221172409) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.datetime "start_date_time"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_lessons_on_user_id", using: :btree
   end
 
