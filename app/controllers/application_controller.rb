a2ed9fd29f9425fd6046ac5a99d1f1bd0c56f465
stephen choi <stephenjhchoi@gmail.com>
2017-02-24 16:48:04 +0000

    changed number inbox

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   after_filter :store_location
+  helper_method :booking_count
 
   def store_location
     # store last url as long as it isn't a /users path
@@ -11,4 +12,16 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     session[:previous_url] || root_path
   end
+
+
+  def booking_count
+    @lessons = current_user.lessons
+    counter = 0
+    @lessons.each do |lesson|
+    counter += 1 if lesson.booking
+    end
+    return counter
+
+  end
+
 end
